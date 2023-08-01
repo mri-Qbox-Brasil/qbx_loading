4f93e51cf158e001ba848c9d7848222c405c4f4e
David Malchin <malchin459@gmail.com>
2023-08-01 12:42:34 +0300

    fix: loading bar restarting (#4)

--- html/app.js
+++ html/app.js
@@ -97,28 +97,9 @@ function videotoggle() {
     }
 }
 
-let count = 0;
-let thisCount = 0;
-
 const handlers = {
-    startInitFunctionOrder(data) {
-        count = data.count;
-    },
-
-    initFunctionInvoking(data) {
-        document.querySelector(".thingy").style.left = "0%";
-        document.querySelector(".thingy").style.width = (data.idx / count) * 100 + "%";
-    },
-
-    startDataFileEntries(data) {
-        count = data.count;
-    },
-
-    performMapLoadFunction(data) {
-        ++thisCount;
-
-        document.querySelector(".thingy").style.left = "0%";
-        document.querySelector(".thingy").style.width = (thisCount / count) * 100 + "%";
+    loadProgress({ loadFraction }) {
+        document.querySelector(".thingy").style.width = loadFraction * 100 + "%";
     },
 };
 
